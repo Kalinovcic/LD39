844e890f403b98842bb46d7779cec0b49006692a
kalinovcic <lovro.kalinovcic@gmail.com>
2017-07-29 08:08:25 +0200

    Robot

--- src/data.h
+++ src/data.h
@@ -8,7 +8,7 @@ Sound sound_fireball;
 
 void load_data()
 {
-    create_mesh("data/models/penguin.obj", &penguin);
+    create_mesh("data/models/robot.obj", &penguin);
 
     load_sound(&sound_fireball, "data/audio/fireball.wav");
 }
