c24450237e68ab9d969cddd39eab249481e27050
kalinovcic <lovro.kalinovcic@gmail.com>
2017-07-30 14:58:39 +0200

    ..

--- build_web.bat
+++ build_web.bat
@@ -1,3 +1,3 @@
 @echo off
-rem em++ -std=c++11 -O2 src/unity_build.cpp -o ld39.html -s USE_SDL=2 -s TOTAL_MEMORY=67108864 --preload-file data
-em++ -std=c++11 src/unity_build.cpp -o ld39.html -s USE_SDL=2 -s TOTAL_MEMORY=67108864 --embed-file data
+em++ -std=c++11 -O2 src/unity_build.cpp -o ld39.html -s USE_SDL=2 -s TOTAL_MEMORY=67108864 --preload-file data
+rem em++ -std=c++11 src/unity_build.cpp -o ld39.html -s USE_SDL=2 -s TOTAL_MEMORY=67108864 --embed-file data
